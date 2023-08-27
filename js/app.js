f40c985e887f2a831edcd972cb4506d3387af014
Noah-Sfez <116373700+Noah-Sfez@users.noreply.github.com>
2023-08-27 12:58:05 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+  
+    navbarToggler.addEventListener("click", function () {
+      navbarCollapse.classList.toggle("show");
+    });
+  
+    // Fermer le menu lorsqu'un lien est cliqué
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    navLinks.forEach(function (link) {
+      link.addEventListener("click", function () {
+        navbarCollapse.classList.remove("show");
+      });
+    });
+  });
+  
